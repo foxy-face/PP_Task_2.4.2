58d247c8e5e0f4fc2f6a0e863ed673d703f2d8d4
foxy_face <nataliasizykh.ns@gmail.com>
2022-01-15 16:54:32 +0300

    Version 15.01.2022

--- src/main/java/web/model/Role.java
+++ src/main/java/web/model/Role.java
@@ -1,15 +1,33 @@
 package web.model;
 
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
 
 // Этот класс реализует интерфейс GrantedAuthority, в котором необходимо переопределить только один метод getAuthority() (возвращает имя роли).
 // Имя роли должно соответствовать шаблону: «ROLE_ИМЯ», например, ROLE_USER.
+@Entity
+@Table(name = "roles")
 public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "role")
     private String role;
 
-    public Role(Long id, String role) {
-        this.id = id;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_role"
+            , joinColumns = @JoinColumn(name = "role_id")
+            , inverseJoinColumns = @JoinColumn(name = "user_id"
+    ))
+    private Set<User> user;
+
+    public Role(String role) {
         this.role = role;
     }
 
@@ -28,6 +46,10 @@ public class Role implements GrantedAuthority {
     public void setRole(String role) {
         this.role = role;
     }
+
+    public Set<User> getUser() { return user; }
+
+    public void setUser(Set<User> user) { this.user = user; }
 
     @Override
     public String getAuthority() {
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -2,17 +2,32 @@ package web.model;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import web.model.Role;
-
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Set;
 
 // Для того, чтобы в дальнейшим использовать класс User в Spring Security, он должен реализовывать интерфейс UserDetails.
 // UserDetails можно представить, как адаптер между БД пользователей и тем что требуется Spring Security внутри SecurityContextHolder
+@Entity
+@Table(name = "users")
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name; // уникальное значение
+
+    @Column(name = "password")
     private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_role"
+            , joinColumns = @JoinColumn(name = "user_id")
+            , inverseJoinColumns = @JoinColumn(name = "role_id"
+    ))
     private Set<Role> roles;
 
     public User() {
